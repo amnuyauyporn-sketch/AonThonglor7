ac67e7a94838e5cba85f4afc2f348fd844325cf0
aontl7 <amnuy.auyporn@gmail.com>
2026-03-23 08:15:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyLRV9OSMwTfBdIUbfpqpkKRVcO9ZNV4M9HNU48R67X3Je2821vQ54F4uEGtnEuLZh5/exec";e
+const API_URL = "https://script.google.com/macros/s/AKfycbwis2RLc-635tXep3nqoDz_cw19KEvVLmQHHDkw5FCkJRzs0hmArJn5WMOFOaDxX4Oo/exec";e
